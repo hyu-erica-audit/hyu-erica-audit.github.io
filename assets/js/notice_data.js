cb9df63141db4935a3925d24f5d64f1d7e8607de
NaCha4 <dnacha4647@gmail.com>
2025-12-30 13:18:29 +0900

    공지사항 기능 추가 중

---
+++ assets/js/notice_data.js
@@ -0,0 +1,41 @@
+/*
+
+위쪽이 최신글임
+
+id: 공지사항 순서 1부터 증가
+type: 배지 타입 (필독, 일반)
+title: 공지사항 제목
+author: 작성자
+date: 작성일 (YYYY.MM.DD 형식)
+link: 공지사항 상세페이지 링크
+
+형식 맞춰서 작성하세요
+
+*/
+
+const noticeList = [
+    {
+        id: 3,
+        type: "필독",
+        title: "2026학년도 상반기 중앙감사 자료 제출 안내",
+        author: "중앙감사위",
+        date: "2026.01.15",
+        link: "/pages/notice/view.html?id=3",
+    },
+    {
+        id: 2,
+        type: "일반",
+        title: "제7대 중앙감사위원회 신입 국원 모집 결과 발표",
+        author: "중앙감사위",
+        date: "2026.01.10",
+        link: "#",
+    },
+    {
+        id: 1,
+        type: "일반",
+        title: "홈페이지 오픈 안내",
+        author: "관리자",
+        date: "2025.12.30",
+        link: "#",
+    }
+];
